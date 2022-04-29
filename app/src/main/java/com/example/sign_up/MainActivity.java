34215b084d794552997d4837a7e6cfc84f6057e1
Arslan Khan <bsef19a018@pucit.edu.pk>
2022-04-29 00:07:27 -0700

    Alert Message

--- app/src/main/java/com/example/sign_up/MainActivity.java
+++ app/src/main/java/com/example/sign_up/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 btn.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
-
+        Toast.makeText(getApplicationContext(),"Account created Successfully",Toast.LENGTH_SHORT).show();
     }
 });
     }
